7fa28d0bb447679bbbdcf59cefce6b4c7b57aa6d
YukaLangbuana <langbuana.yuka@hotmail.com>
2020-05-22 23:18:43 +0700

    mvp tidy

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
+import json
+import pandas as pd
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import Response, request, send_file
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path='')
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/process', methods=["POST"])
+def process():
+    if "file" in request.files:
+        filename = _cleanup(request.files['file'])
+        return jsonify(file_name=filename), 200
+    else:
+        return jsonify(message='Something happened'), 500
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_file(filename, as_attachment=True)
+
+def _cleanup(file):
+    converted_filename = (file.filename).split('.')[0]+'-converted.xls'
+    df1 = pd.read_excel(file)
+    df2 = []
+    for item in df1['good_description']:
+        df2.append(json.loads(item))
+    df2 = pd.DataFrame(df2)
+    df3 = pd.merge(df1, df2, left_index=True, right_index=True)
+    writer = pd.ExcelWriter(converted_filename)
+    df3.to_excel(writer)
+    writer.save()
+    return converted_filename
